15d89b9f2144ff01ee627c2995e26f41f4846a5f
myway <praveencls@yahoo.com>
2014-11-16 08:52:49 +0530

    Website Basic Solution 1. Mvc Application 2. WebServices 3. WebAPI 4. Unicorn. 5. Courier 6. ship 7. Glimpse

---
+++ Website/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Website
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+           // routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           // routes.IgnoreRoute("{resource}.svc/{*pathInfo}");
+            //AreaRegistration.RegisterAllAreas();
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
