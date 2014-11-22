07b45b10264777687bdcbbc715fb096e4de0593e
myway <praveencls@yahoo.com>
2014-11-22 19:13:11 +0530

    ViewModels

---
+++ Website/ViewModels/DemoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Mvc.Presentation;
+
+namespace Website.ViewModels
+{
+    public class DemoViewModel : IRenderingModel
+    {
+        #region IRenderingModel Members
+
+        public void Initialize(Rendering rendering)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
