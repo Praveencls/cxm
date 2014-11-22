6128fabf33c53de2dad1497c6c951b10179b4f0e
myway <praveencls@yahoo.com>
2014-11-22 19:12:33 +0530

    xml serialization in web api

---
+++ Website/CoreLib/WebItemAPI/SwitchToXmlSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.ItemWebApi.Pipelines.Request;
+using Sitecore.ItemWebApi;
+
+namespace Website.CoreLib.WebItemAPI
+{
+    public class SwitchToXmlSerializer : RequestProcessor
+    {
+        public override void Process([Sitecore.NotNull] RequestArgs arguments)
+        {
+            if (System.Web.HttpContext.Current.Request.QueryString["sc_type"] == "xml")
+            {
+                Context.Current.Serializer = new XmlSerializer();
+            }
+        }
+    }
+}
