2d6d28981228078c42c0b8781121592e9bf53b4f
myway <praveencls@yahoo.com>
2014-11-27 07:36:40 +0530

    add corelib project

---
+++ CoreLib/WebItemAPI/SwitchToXmlSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.ItemWebApi.Pipelines.Request;
+using Sitecore.ItemWebApi;
+
+namespace CoreLib.WebItemAPI
+{
+    public class SwitchToXmlSerializer : RequestProcessor
+    {
+        public override void Process([Sitecore.NotNull] RequestArgs arguments)
+        {
+            if (System.Web.HttpContext.Current.Request.QueryString["type"] == "xml")
+            {
+                Context.Current.Serializer = new XmlSerializer();
+            }
+        }
+    }
+}
