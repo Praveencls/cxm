a4d9c3e4ea976a23ff4d8a389d8b696aecaae3cd
myway <praveencls@yahoo.com>
2014-11-27 07:45:41 +0530

    done with some glass mapper classes

---
+++ Website/App_Start/GlassMapperScRazor.cs
@@ -0,0 +1,42 @@
+/*************************************
+
+DO NOT CHANGE THIS FILE - UPDATE GlassMapperScCustom.cs
+
+**************************************/
+
+using System;
+using System.Linq;
+using Glass.Mapper.Sc.CastleWindsor;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Razor;
+using Glass.Mapper.Configuration.Attributes;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Website.App_Start.GlassMapperScRazor), "Start")]
+
+namespace Website.App_Start
+{
+	public static class  GlassMapperScRazor
+	{
+		public static void Start()
+		{
+			//create config
+			var config = new Config();
+
+			//create the resolver
+			var resolver = DependencyResolver.CreateStandardResolver();
+
+			resolver.Container.Install(new SitecoreInstaller(config));
+
+			//create a context
+			var context = Glass.Mapper.Context.Create(resolver, GlassRazorSettings.ContextName);
+
+			var loader = new AttributeConfigurationLoader("Glass.Mapper.Sc.Razor");
+
+			context.Load(       
+				loader       				
+				);
+
+			
+		}
+	}
+}
